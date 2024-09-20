53de65d47e003af0ad4ef0c8fe1ca31149ead97e
joelr-2002 <119701694+joelr-2002@users.noreply.github.com>
2024-09-20 13:30:43 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Docker PHP 7.4</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
     <div class="bg-white p-6 rounded-lg shadow-lg text-center" x-data="{ message: 'Si ve este archivo es porque el Docker con PHP 7.4 está funcionando correctamente en el puerto 8081' }">
